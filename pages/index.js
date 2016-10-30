75c46150093c9f7a922bed54fc5edfe0a561f154
Gavin Mogan <gavin@gavinmogan.com>
2016-10-30 15:39:36 -0700

    main page should be projects

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,13 @@
-import React from 'react'
-import Head from 'next/head'
-import css from 'next/css'
+import React from 'react';
+import Projects from './projects';
 
-const style = css({
-  color: 'red',
-  ':hover': {
-    color: 'blue'
-  },
-  '@media (max-width: 500px)': {
-    color: 'rebeccapurple'
+export default class Index extends React.Component {
+  static get propTypes () {
+    return {
+      url: React.PropTypes.object.isRequired
+    };
   }
-})
-
-export default () => (
-  <div>
-    <Head>
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-    </Head>
-    <h1 className={style}>Hi. I'm mobile-ready!</h1>
-  </div>
-)
+  render () {
+    return <Projects url={this.props.url} />;
+  }
+}
