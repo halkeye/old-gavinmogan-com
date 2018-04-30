9808bb610b02d5fd52ff45db18808c40135a9ebc
Gavin Mogan <git@gavinmogan.com>
2018-04-29 19:15:49 -0700

    swap out llc with nodeschool in volunteering section

--- pages/volunteering.js
+++ pages/volunteering.js
@@ -17,11 +17,22 @@ export class Volunteering extends React.Component {
         <Header title='Volunteering And Hangouts' description='Where does Gavin hangout?' pathname={this.props.router.pathname} />
         <div className="container">
           <h1>Volunteering and Hangouts</h1>
-          <ItemBlock key="ladies-learning-code"
-            img="https://pbs.twimg.com/profile_images/614135444775825409/GHWE979-_reasonably_small.png"
-            link="http://ladieslearningcode.com/chapters/vancouver/"
-            title="Ladies Learning Code Vancouver"
-          />
+
+          <ItemBlock key="nodeschool-yvr"
+            img="/static/img/volunteering/nodeschool-yvr.png"
+            link="https://community.vancouvertech.com/groups/nodeschool"
+            title="NodeSchool Vancouver"
+          >
+            <p>
+              Help out where I can. Usually finding a group that wants to do a certain excersize at our events and help mentor them.
+            </p>
+            <p>
+              <a href="https://community.vancouvertech.com/groups/nodeschool">forum</a>{' | '}
+              <a href="https://github.com/nodeschool/vancouver">github</a>{' | '}
+              <a href="https://meetup.com/nodeschool-vancouver">meetup</a>{' | '}
+              <a href="https://twitter.com/nodeschoolyvr">twitter</a>
+            </p>
+          </ItemBlock>
 
           <ItemBlock key="yvr-dev"
             link="https://yvrdev.herokuapp.com"
